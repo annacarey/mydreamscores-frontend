9d446fbba5d246431e8489fddce6f210806ead33
annacarey <annacarey@icloud.com>
2020-03-18 14:09:43 -0400

    added journal creation

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,7 +1,8 @@
+// Log in user
 const getUserActionCreator = (email, password) => dispatch => {
     dispatch(getUserStarted())
 
-    fetch('http://localhost:3000/login', {
+    return fetch('http://localhost:3000/login', {
         method: "POST",
         headers: {'content-type': 'application/json',
             'accept': 'application/json'},
@@ -10,9 +11,7 @@ const getUserActionCreator = (email, password) => dispatch => {
         })
     }).then((response) => response.json())
       .then((user) => {
-          setTimeout(() => {
-            return user.error? dispatch(getUserFailed(user.error)) : dispatch(getUserSuccess(user)), 2000
-        })
+        user.error? dispatch(getUserFailed(user.error)) : dispatch(getUserSuccess(user))
       })
 }
 
@@ -20,28 +19,91 @@ const getUserStarted = () => ({
     type: 'GET_USER_STARTED'
 })
 
-const getUserSuccess = (user) => ({
-    type: 'GET_USER_SUCCESS', 
-    payload: {user}
-})
+const getUserSuccess = (user) => {
+    return ({
+        type: 'GET_USER_SUCCESS', 
+        payload: {user}
+    })
+}
 
 const getUserFailed = (errors) => ({
     type: 'GET_USER_FAILED',
     payload: {errors}
 })
 
-const setZipcodeActionCreator = zipCode => ({
-    type: 'SET_USER_ZIPCODE',
-    payload: {zipCode}
+// Sign up user
+const signupUserActionCreator = (userInfo) => dispatch => {
+    dispatch(signupUserStarted())
+
+    return fetch('http://localhost:3000/signup', {
+        method: "POST",
+        headers: {'content-type': 'application/json',
+            'accept': 'application/json'},
+        body: JSON.stringify({ user:
+            userInfo
+        })
+    }).then((response) => response.json())
+      .then((user) => {
+            user.error? dispatch(signupUserFailed(user.error)) : dispatch(signupUserSuccess(user))
+            return user
+      })
+}
+
+const signupUserStarted = () => ({
+    type: 'SIGNUP_USER_STARTED'
 })
 
-const timesUpActionCreator = () => ({
-    type: 'TIMES_UP'
+const signupUserSuccess = (user) => {
+    return ({
+        type: 'SIGNUP_USER_SUCCESS', 
+        payload: {user}
+    })
+}
+
+const signupUserFailed = (errors) => ({
+    type: 'SIGNUP_USER_FAILED',
+    payload: {errors}
 })
 
-const updateCurrentJournalEntry = (content) => ({
-    type: 'TIMES_UP',
-    payload: {content}
+const addJournalEntryActionCreator = (content, zipcode, user) => (dispatch) => {
+    dispatch(addJournalEntryStarted())
+    return fetch('http://localhost:3000/journal-entries', {
+        method: "POST",
+        headers: {'content-type': 'application/json',
+            'accept': 'application/json'},
+        body: JSON.stringify({ content, zipcode, user
+        })
+        }).then((response) => response.json())
+        .then((journalEntry) => {
+            dispatch(addJournalEntry(journalEntry))
+        })
+}
+
+const addJournalEntryStarted = () => ({
+    type: 'ADD_JOURNAL_ENTRY_STARTED'
 })
 
-export {getUserActionCreator, setZipcodeActionCreator, timesUpActionCreator, updateCurrentJournalEntry}
+const addJournalEntry = journalEntry => ({
+    type: 'ADD_JOURNAL_ENTRY',
+    payload: {journalEntry}
+})
+
+const updateJournalEntryRequest = (userId, journalEntryId) => dispatch => {
+    fetch(`http://localhost:3000/journal-entries/${journalEntryId}`, {
+        method: "PATCH",
+        headers: {'content-type': 'application/json',
+            'accept': 'application/json'},
+        body: JSON.stringify({ userId, journalEntryId
+        })
+        }).then((response) => response.json())
+        .then((journalEntry) => {
+            dispatch(updateJournalEntry(journalEntry))
+        })
+}
+
+const updateJournalEntry = (journalEntry) => ({
+    type: 'UPDATE_JOURNAL_ENTRY',
+    payload: {journalEntry}
+})
+
+export {getUserActionCreator, signupUserActionCreator, addJournalEntryActionCreator, updateJournalEntryRequest}
--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -2,16 +2,32 @@ import React from 'react';
 import UserLogin from '../components/UserLogin'
 import SurveyContainer from './SurveyContainer'
 import Dashboard from './Dashboard'
-import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
+import Welcome from '../components/Welcome'
+import Journal from '../components/Journal'
+import SentimentView from '../components/SentimentView'
+import Graph from '../components/Graph'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class UserContainer extends React.Component {
+
+    state = {
+        zipcode: ""
+    }
+
+    setZipcode = (zipcode) => {
+        this.setState(() => { return {zipcode: zipcode}})
+    }
 
     render() {
         return (
             <div>
                 <Switch>
-                    <Route exact path='/login' component={UserLogin} />
-                    <Route exact path = '/dashboard' component={Dashboard} />
+                    <Route exact path ="/" render={ (props) => <Welcome {...props} setZipcode={this.setZipcode}/> } /> 
+                    <Route exact path='/login' render={ () =><UserLogin /> } />
+                    <Route exact path = '/dashboard' render={ () =><Dashboard /> }  />
+                    <Route exact path='/journal' render={() => <Journal zipcode={this.state.zipcode}/>} />
+                    <Route exact path = '/sentiment' render={ () =><SentimentView zipcode={this.state.zipcode} /> }  />
+                    <Route exact path = '/graph' render={ () =><Graph /> }  />
                     <SurveyContainer />
                 </Switch>
             </div>
@@ -19,4 +35,4 @@ class UserContainer extends React.Component {
     }
 }
 
-export default withRouter(UserContainer)
+export default UserContainer
